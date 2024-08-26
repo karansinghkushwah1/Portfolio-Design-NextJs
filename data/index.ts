48081ebbc58ce4b7c19e6c559b125ca21f367a56
karansinghkushwah1 <kushwah.4@iitj.ac.in>
2024-08-27 01:51:22 +0530

    first commit

---
+++ data/index.ts
@@ -0,0 +1,220 @@
+export const navItems = [
+    { name: "About", link: "#about" },
+    { name: "Projects", link: "#projects" },
+    { name: "Work Experience", link: "#workExperience" },
+    { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+    {
+        id: 1,
+        title: "I prioritize client collaboration, fostering open communication ",
+        description: "",
+        className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+        imgClassName: "w-full h-full",
+        titleClassName: "justify-end",
+        img: "/b1.svg",
+        spareImg: "",
+    },
+    {
+        id: 2,
+        title: "I'm very flexible with time zone communications",
+        description: "",
+        className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+        imgClassName: "",
+        titleClassName: "justify-start",
+        img: "",
+        spareImg: "",
+    },
+    {
+        id: 3,
+        title: "My tech stack",
+        description: "I constantly try to improve",
+        className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+        imgClassName: "",
+        titleClassName: "justify-center",
+        img: "",
+        spareImg: "",
+    },
+    {
+        id: 4,
+        title: "Tech enthusiast with a passion for development.",
+        description: "",
+        className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+        imgClassName: "",
+        titleClassName: "justify-start",
+        img: "/grid.svg",
+        spareImg: "/b4.svg",
+    },
+
+    {
+        id: 5,
+        title: "Currently improving websites designs",
+        description: "Cooking...",
+        className: "md:col-span-3 md:row-span-2",
+        imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+        titleClassName: "justify-center md:justify-start lg:justify-center",
+        img: "/b5.svg",
+        spareImg: "/grid.svg",
+    },
+    {
+        id: 6,
+        title: "Do you want to start a project together?",
+        description: "",
+        className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+        imgClassName: "",
+        titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+        img: "",
+        spareImg: "",
+    },
+];
+
+export const projects = [
+    {
+        id: 1,
+        title: "KBVidya - Classroom and Learning Management System",
+        des: "Explore the new and updated classroom management system with integrated assignments and test support, build using NextJs and Firebase",
+        img: "/kbvidya.jpg",
+        iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/firebase.svg"],
+        link: "https://www.kbvidya.com/",
+    },
+    {
+        id: 2,
+        title: "Realestate App - Sell & Buy Properties",
+        des: "A dynamic and super easy website to navigate properties available for sell and buy. Build using ReactJs and Restful API",
+        img: "/realestate.jpg",
+        iconLists: ["/re.svg", "/tail.svg", "/ts.svg","/rest.svg"],
+        link: "https://real-estate-app-karan-gamma.vercel.app/",
+    },
+    {
+        id: 3,
+        title: "Inventory and asset management platform",
+        des: "A modern and easy-to-use website designed for asset management, built using Next.js and Firebase for efficient performance and seamless functionality.",
+        img: "/kbassetmanagement.jpg",
+        iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/firebase.svg"],
+        link: "https://www.kbassetmanagement.com/",
+    },
+    {
+        id: 4,
+        title: "A Blogging Platform - TalentServe",
+        des: "A sleek and interactive blogging platform built with React.js, Node.js and Mondodb, offering a seamless experience for creating, managing, and sharing content with ease.",
+        img: "/blogging.jpg",
+        iconLists: ["/re.svg", "/tail.svg", "/js.svg", "/mongodb.svg"],
+        link: "https://github.com/karansinghkushwah1/TalentServe_Blogging_Platform",
+    },
+];
+
+export const testimonials = [
+    {
+        quote:
+            "Collaborating with Karan was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Karan's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Karan is the ideal partner.",
+        name: "Michael Johnson",
+        title: "Director of AlphaStream Technologies",
+    },
+    {
+        quote:
+            "Collaborating with Karan was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Karan's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Karan is the ideal partner.",
+        name: "Michael Johnson",
+        title: "Director of AlphaStream Technologies",
+    },
+    {
+        quote:
+            "Collaborating with Karan was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Karan's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Karan is the ideal partner.",
+        name: "Michael Johnson",
+        title: "Director of AlphaStream Technologies",
+    },
+    {
+        quote:
+            "Collaborating with Karan was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Karan's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Karan is the ideal partner.",
+        name: "Michael Johnson",
+        title: "Director of AlphaStream Technologies",
+    },
+    {
+        quote:
+            "Collaborating with Karan was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Karan's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Karan is the ideal partner.",
+        name: "Michael Johnson",
+        title: "Director of AlphaStream Technologies",
+    },
+];
+
+export const companies = [
+    {
+        id: 1,
+        name: "cloudinary",
+        img: "/cloud.svg",
+        nameImg: "/cloudName.svg",
+    },
+    {
+        id: 2,
+        name: "appwrite",
+        img: "/app.svg",
+        nameImg: "/appName.svg",
+    },
+    {
+        id: 3,
+        name: "HOSTINGER",
+        img: "/host.svg",
+        nameImg: "/hostName.svg",
+    },
+    {
+        id: 4,
+        name: "stream",
+        img: "/s.svg",
+        nameImg: "/streamName.svg",
+    },
+    {
+        id: 5,
+        name: "docker.",
+        img: "/dock.svg",
+        nameImg: "/dockerName.svg",
+    },
+];
+
+export const workExperience = [
+    {
+        id: 1,
+        title: "Software Engineer Intern - Odoo",
+        desc: "Currently working with Owl framewark, javascript and python to improving Odoo",
+        className: "md:col-span-2",
+        thumbnail: "/exp1.svg",
+    },
+    {
+        id: 2,
+        title: "Full stack Dev - KBCloudTech",
+        desc: "Designed and developed websites for class management system and other compeny need using Nextjs and Firebase",
+        className: "md:col-span-2", // change to md:col-span-2
+        thumbnail: "/exp2.svg",
+    },
+    {
+        id: 3,
+        title: "Software Engineer Intern - TalentServe",
+        desc: "Led the dev of a blogging application for a company's need, from initial concept to deployment.",
+        className: "md:col-span-2", // change to md:col-span-2
+        thumbnail: "/exp3.svg",
+    },
+    {
+        id: 4,
+        title: "Freelance Frontend Developer",
+        desc: "Developed and maintained user-facing features using modern frontend technologies.",
+        className: "md:col-span-2",
+        thumbnail: "/exp4.svg",
+    },
+];
+
+export const socialMedia = [
+    {
+        id: 1,
+        img: "/git.svg",
+        link: "https://github.com/karansinghkushwah21",
+    },
+    {
+        id: 2,
+        img: "/twit.svg",
+        link: "",
+    },
+    {
+        id: 3,
+        img: "/link.svg",
+        link: "",
+    },
+];
